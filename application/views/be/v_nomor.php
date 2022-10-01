071e4f84f92b3a3c54657c9cb34090ccff0f4e97
roy <royw50855@gmail.com>
2022-10-01 21:34:02 +0800

    v_nomor finish

--- application/views/be/v_nomor.php
+++ application/views/be/v_nomor.php
@@ -1,3 +1,46 @@
+<?php 
+ function getRomawi($bln)
+  {
+    switch ($bln) {
+      case 1:
+        return "I";
+        break;
+      case 2:
+        return "II";
+        break;
+      case 3:
+        return "III";
+        break;
+      case 4:
+        return "IV";
+        break;
+      case 5:
+        return "V";
+        break;
+      case 6:
+        return "VI";
+        break;
+      case 7:
+        return "VII";
+        break;
+      case 8:
+        return "VIII";
+        break;
+      case 9:
+        return "IX";
+        break;
+      case 10:
+        return "X";
+        break;
+      case 11:
+        return "XI";
+        break;
+      case 12:
+        return "XII";
+        break;
+    }
+  }
+?>
 <!-- body -->
 <div class="content-wrapper">
   <!-- Content Header (Page header) -->
